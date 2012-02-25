1fadfc35ebc74914c4fd0bebd561f4677d66ad2f
Michael <ceo.wonu@gmail.com>
2012-02-24 22:18:51 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysneaksWonu::Application.config.secret_token = '8a8323693f0ba1f92eeb0f84a3aa728f1780e25b8de087b103677df760314f0dbd5b04b262c654ea670950591a9f7f8004d47f04da80001ad1317c1febb86842'
